0eba4e1a6f91075a0825e9f675513003c4553122
CristianPeter <horatiucristianpeter@gmail.com>
2025-08-27 20:42:30 +0200

    bugfix: update logic from EnumLogger

--- src/Support/EnumLogger.php
+++ src/Support/EnumLogger.php
@@ -11,28 +11,27 @@ final class EnumLogger
     public static function logOnFail(string $enumClass, string $property): Callback
     {
         return new Callback(function (string|int $value) use ($enumClass, $property): bool {
-            if ( ! enum_exists($enumClass)) {
-                return true;
-            }
-            $reflection = new ReflectionEnum($enumClass);
-            if ( ! $reflection->isBacked()) {
-                return true;
-            }
+            if (enum_exists($enumClass)) {
+                $reflection = new ReflectionEnum($enumClass);
+                if ( ! $reflection->isBacked()) {
+                    return true;
+                }
 
-            if ($reflection->isBacked()) {
-                /** @var class-string<BackedEnum> $enumClass */
-                if (null === $enumClass::tryFrom($value)) {
-                    $logValue = is_string($value) ? $value : var_export($value, true);
-                    Logger::getInstance()->warning(sprintf(
-                        'Unexpected value on deserialization in %s::%s => %s',
-                        $enumClass,
-                        $property,
-                        $logValue,
-                    ), [
-                        'class' => $enumClass,
-                        'property' => $property,
-                        'value' => $logValue,
-                    ]);
+                if ($reflection->isBacked()) {
+                    /** @var class-string<BackedEnum> $enumClass */
+                    if (null === $enumClass::tryFrom($value)) {
+                        $logValue = is_string($value) ? $value : var_export($value, true);
+                        Logger::getInstance()->warning(sprintf(
+                            'Unexpected value on deserialization in %s::%s => %s',
+                            $enumClass,
+                            $property,
+                            $logValue,
+                        ), [
+                            'class' => $enumClass,
+                            'property' => $property,
+                            'value' => $logValue,
+                        ]);
+                    }
                 }
             }
 
